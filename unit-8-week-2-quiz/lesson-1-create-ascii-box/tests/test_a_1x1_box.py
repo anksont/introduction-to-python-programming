19051413e805a62f33e5acd96fe2da708af5b628
Santiago Basulto <santiago.basulto@gmail.com>
2017-11-24 00:26:54 -0300

    Test Names

--- unit-8-week-2-quiz/lesson-1-create-ascii-box/tests/test_a_1x1_box.py
+++ unit-8-week-2-quiz/lesson-1-create-ascii-box/tests/test_a_1x1_box.py
@@ -2,5 +2,5 @@ box_1x1_expected = """
 @
 """.lstrip()
 
-def test_second_box():
+def test_a_1x1_box():
     assert create_box(1, 1, '@') == box_1x1_expected
--- unit-8-week-2-quiz/lesson-1-create-ascii-box/tests/test_a_3x4_box.py
+++ unit-8-week-2-quiz/lesson-1-create-ascii-box/tests/test_a_3x4_box.py
@@ -4,5 +4,5 @@ box_3x4_expected = """
 ****
 """.lstrip()
 
-def test_a_3_x_4_box():
+def test_a_3x4_box():
     assert create_box(3, 4, '*') == box_3x4_expected
