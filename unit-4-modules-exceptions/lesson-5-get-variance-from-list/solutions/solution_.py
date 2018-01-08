9a0aa599f883ef5038e02cb597c8ece9cd029eec
jasonmeeks <jasonmeeks42@gmail.com>
2018-01-08 10:19:04 -0600

    im starting to hate py2

--- unit-4-modules-exceptions/lesson-5-get-variance-from-list/solutions/solution_.py
+++ unit-4-modules-exceptions/lesson-5-get-variance-from-list/solutions/solution_.py
@@ -3,8 +3,8 @@ try:
   from statistics import variance
 except ImportError:
   def variance(a_list):
-    mean = sum(a_list) / len(a_list)
-    return sum([(x - mean) ** 2 for x in a_list]) / (len(a_list) - 1)
+    mean = sum(a_list) / float(len(a_list))
+    return sum([(x - mean) ** 2 for x in a_list]) / float((len(a_list) - 1))
 
 def get_variance_from_list(a_list):
     return variance(a_list)
